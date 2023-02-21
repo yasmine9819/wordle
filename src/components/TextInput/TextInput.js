efbf68a0028a64eef8206aec24c4f46cc362ca11
yasmine9819 <yasminemustapha98@gmail.com>
2023-02-21 20:28:49 +0100

    feat: first exercise

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function TextInput() {
+  const [guess, setGuess] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("this is your guess: ", guess);
+    setGuess("");
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        pattern="\w{5,5}"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default TextInput;
